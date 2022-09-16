10f55cddca494f07e8c3e9ebb70e9c57450cb91a
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-09-16 20:38:15 +0100

    chore: add src

---
+++ src/poetry_dotenv/__init__.py
@@ -0,0 +1,13 @@
+"""poetry-dotenv - is the tool that automatically loads environment variables from .env files into the environment before poetry commands are run."""
+
+__title__ = "poetry-dotenv"
+__summary__ = "poetry-dotenv - is the tool that automatically loads environment variables from .env files into the environment before poetry commands are run."
+__uri__ = "https://poetry-dotenv.readthedocs.io/en/latest"
+
+__version__ = "0.0.1"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, Volodymyr Pivoshenko"
