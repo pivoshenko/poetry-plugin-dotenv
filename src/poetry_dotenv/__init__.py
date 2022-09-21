2adb28048cb1e8809f1d318a7344a7b00c70147f
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-09-21 21:04:01 +0100

    build(version): bumped version

--- src/poetry_dotenv/__init__.py
+++ src/poetry_dotenv/__init__.py
@@ -4,7 +4,7 @@ __title__ = "poetry-dotenv"
 __summary__ = "poetry-dotenv - is the plugin that automatically loads environment variables from a dotenv file into the environment before poetry commands are run."
 __uri__ = "https://poetry-dotenv.readthedocs.io/en/latest"
 
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 
 __author__ = "Volodymyr Pivoshenko"
 __email__ = "volodymyr.pivoshenko@gmail.com"
