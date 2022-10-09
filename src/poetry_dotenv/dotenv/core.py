a7123db2a2978ce060872a8d0d3c375ea52eee88
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-10-09 21:55:51 +0100

    chore: reset

--- src/poetry_dotenv/dotenv/core.py
+++ src/poetry_dotenv/dotenv/core.py
@@ -1,12 +1,10 @@
 """Module that contains core dotenv IO functionality."""
 
 import re
+import sys
 
 from pathlib import Path
-import sys
-from types import FrameType
 from typing import Iterator
-from typing import Optional
 from typing import Pattern
 
 
@@ -22,10 +20,10 @@ def walk_to_root(path: Path) -> Iterator[Path]:
     if not path.exists():
         raise IOError("Starting path {0!r} not found.".format(path))
 
-    dirpath: Path = path.parent if path.is_file() else path
+    dirpath = path.parent if path.is_file() else path
 
-    last_dir: Optional[Path] = None
-    current_dir: Path = dirpath.absolute()
+    last_dir = None
+    current_dir = dirpath.absolute()
 
     while last_dir != current_dir:
         yield current_dir
@@ -36,21 +34,44 @@ def walk_to_root(path: Path) -> Iterator[Path]:
 
 def find_dotenv(
     filename: str = ".env",
-    raise_error_if_not_found: bool = False,
     usecwd: bool = False,
+    raise_error_if_not_found: bool = False,
 ) -> Path:
     """Search in increasingly higher directories for the given file."""
 
+    path = _get_path(usecwd)
+
+    for dirname in walk_to_root(path):
+        check_path = dirname / filename
+
+        if check_path.exists():
+            return check_path
+
+    if raise_error_if_not_found:
+        raise IOError("File {0!r} not found.".format(filename))
+
+    return Path()
+
+
+def _get_path(usecwd: bool = False) -> Path:
+    """Get a base path."""
+
     if usecwd or getattr(sys, "frozen", False):
-        path: Path = Path.cwd()
+        path = Path.cwd().absolute()
 
     else:
-        frame: FrameType = sys._getframe()  # noqa: WPS437
-        current_file: str = __file__
+        # WPS437 - the protected method is the only way to get a frame object from the call stack
+        frame = sys._getframe()  # noqa: WPS437
+        current_file = __file__
 
         while frame.f_code.co_filename == current_file:
-            assert frame.f_back is not None  # noqa: S101
-            frame = frame.f_back
+            if frame.f_back:
+                frame = frame.f_back
+
+            else:
+                raise ValueError("Frame {0!r} is None.".format(frame.f_back))
 
         frame_filename = frame.f_code.co_filename
-        path = os.path.dirname(os.path.abspath(frame_filename))
+        path = Path(frame_filename).absolute()
+
+    return path
