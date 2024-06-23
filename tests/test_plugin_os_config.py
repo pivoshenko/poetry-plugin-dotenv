245c326a80bc05be435c42bfcdbace5f793198ee
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2024-06-23 20:26:51 +0100

    test: split tests based on the config source

--- tests/test_plugin_os_config.py
+++ tests/test_plugin_os_config.py
@@ -1,10 +1,13 @@
-"""Tests for the module ``src/poetry_plugin_dotenv/plugin.py``."""
+"""Tests for the module ``src/poetry_plugin_dotenv/plugin.py`` with OS as config source."""
 
 from __future__ import annotations
 
 import os
 
 from typing import TYPE_CHECKING
+from unittest import mock
+
+import pytest
 
 from poetry.console.commands.env_command import EnvCommand
 
@@ -17,7 +20,33 @@ if TYPE_CHECKING:
     import pytest_mock
 
 
-def test_default_dotenv_file(
+@mock.patch.dict(os.environ, {"POETRY_PLUGIN_DOTENV_LOCATION": ".env.dev"}, clear=True)
+def test_dev_dotenv_file_os_config(
+    mocker: pytest_mock.MockFixture,
+    create_dotenv_file: Callable[[dict[str, str], str], None],
+    remove_dotenv_file: Callable[[str], None],
+) -> None:
+    """Test for the ``load`` method."""
+
+    event = mocker.MagicMock()
+    event.command = EnvCommand()
+    event.io.input.option.return_value = None
+
+    filename = ".env.dev"
+    content = {"POSTGRES_USER": "root"}
+
+    create_dotenv_file(content, filename)
+
+    plugin = DotenvPlugin()
+    plugin.load(event)
+
+    remove_dotenv_file(filename)
+
+    assert content["POSTGRES_USER"] == os.environ["POSTGRES_USER"]
+
+
+@mock.patch.dict(os.environ, {"POETRY_PLUGIN_DOTENV_IGNORE": "y"}, clear=True)
+def test_without_dotenv_file_os_config(
     mocker: pytest_mock.MockFixture,
     create_dotenv_file: Callable[[dict[str, str], str], None],
     remove_dotenv_file: Callable[[str], None],
@@ -38,20 +67,5 @@ def test_default_dotenv_file(
 
     remove_dotenv_file(filename)
 
-    assert content["POSTGRES_USER"] == os.environ["POSTGRES_USER"]
-
-
-def test_dotenv_file_doesnt_exist(
-    mocker: pytest_mock.MockFixture,
-    remove_dotenv_file: Callable[[str], None],
-) -> None:
-    """Test for the ``load`` method."""
-
-    event = mocker.MagicMock()
-    event.command = EnvCommand()
-    event.io.input.option.return_value = None
-
-    remove_dotenv_file(".env")
-
-    plugin = DotenvPlugin()
-    plugin.load(event)
+    with pytest.raises(KeyError):
+        os.environ["POSTGRES_USER"]
