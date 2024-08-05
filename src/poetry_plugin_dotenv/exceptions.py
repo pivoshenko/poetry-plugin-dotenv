80fbf795cab6bebe9cfa1633a2b1a8039ec611bc
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2024-08-05 20:38:56 +0100

    feat: add module for custom exceptions

---
+++ src/poetry_plugin_dotenv/exceptions.py
@@ -0,0 +1,11 @@
+"""Module defines custom exceptions that are used across plugin."""
+
+from __future__ import annotations
+
+
+class PoetryPluginDotenvError(Exception):
+    """Base exception."""
+
+
+class PoetryPluginDotenvPatternError(PoetryPluginDotenvError):
+    """Exception raised when a dotenv pattern is not valid or not found."""
